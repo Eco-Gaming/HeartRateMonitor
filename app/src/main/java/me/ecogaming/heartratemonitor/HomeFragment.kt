e71f695274e91c58bf8601754f0920053a740406
Eco-Gaming <7ecogaming4@gmail.com>
2023-06-16 18:38:17 +0200

    added backend

--- app/src/main/java/me/ecogaming/heartratemonitor/HomeFragment.kt
+++ app/src/main/java/me/ecogaming/heartratemonitor/HomeFragment.kt
@@ -1,13 +1,28 @@
 package me.ecogaming.heartratemonitor
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import me.ecogaming.heartratemonitor.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+
+class HomeFragment : Fragment(), SensorEventListener {
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -15,10 +30,26 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var sensorManager: SensorManager
+    private var heartRateSensor: Sensor? = null
+    private var measure = false
+
+    private val heartRateRequestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                measureHeartRate()
+            } else {
+                showBodySensorRationale(requireContext())
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        sensorManager = requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
@@ -27,10 +58,73 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonMeasureHeartRate.setOnClickListener {
+            measure = !measure
+            if (measure) {
+                measureHeartRate()
+            } else {
+                stopMeasuringHeartRate()
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_HEART_RATE) {
+            val heartRate = event.values[0]
+            binding.textHeartRate.text = getString(R.string.text_heart_rate, heartRate.toInt().toString())
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        if (sensor?.type == Sensor.TYPE_HEART_RATE) {
+            binding.textHeartRateAccuracy.text = getString(R.string.text_heart_rate_accuracy, accuracy.toString())
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopMeasuringHeartRate()
+    }
+
+    private fun measureHeartRate() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.BODY_SENSORS) == PackageManager.PERMISSION_GRANTED) {
+            sensorManager.registerListener(this, heartRateSensor, SensorManager.SENSOR_DELAY_NORMAL)
+            binding.buttonMeasureHeartRate.text = getString(R.string.button_measure_heart_rate_stop)
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.BODY_SENSORS)) {
+                showBodySensorRationale(requireContext())
+            } else {
+                heartRateRequestPermissionLauncher.launch(Manifest.permission.BODY_SENSORS)
+            }
+        }
+    }
+
+    private fun stopMeasuringHeartRate() {
+        sensorManager.unregisterListener(this)
+        binding.textHeartRate.text = getString(R.string.text_heart_rate_idle)
+        binding.textHeartRateAccuracy.text = ""
+        binding.buttonMeasureHeartRate.text = getString(R.string.button_measure_heart_rate)
+    }
+
+    private fun showBodySensorRationale(context: Context) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+        builder.setTitle(R.string.body_sensors_permission_dialog_title)
+            .setMessage(R.string.body_sensors_permission_dialog_message)
+            .setPositiveButton(R.string.body_sensors_permission_dialog_go_to_settings) { _, _ ->
+                // Open app settings
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri =
+                    Uri.fromParts("package", context.packageName, null)
+                intent.data = uri
+                context.startActivity(intent)
+            }
+            .setNegativeButton(R.string.cancel, null)
+            .show()
     }
 }
