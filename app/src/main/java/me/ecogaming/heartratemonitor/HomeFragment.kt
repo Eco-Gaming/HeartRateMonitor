54a20cd524013c52159f119d911531cfef8c478b
Eco-Gaming <7ecogaming4@gmail.com>
2023-06-16 18:54:39 +0200

    very slightly improved UI

--- app/src/main/java/me/ecogaming/heartratemonitor/HomeFragment.kt
+++ app/src/main/java/me/ecogaming/heartratemonitor/HomeFragment.kt
@@ -95,6 +95,9 @@ class HomeFragment : Fragment(), SensorEventListener {
     private fun measureHeartRate() {
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.BODY_SENSORS) == PackageManager.PERMISSION_GRANTED) {
             sensorManager.registerListener(this, heartRateSensor, SensorManager.SENSOR_DELAY_NORMAL)
+            binding.textHeartRate.text = getString(R.string.text_heart_rate, "0")
+            binding.textHeartRate.textSize = 32F
+            binding.textHeartRateAccuracy.text = getString(R.string.text_heart_rate_accuracy, "0")
             binding.buttonMeasureHeartRate.text = getString(R.string.button_measure_heart_rate_stop)
         } else {
             if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.BODY_SENSORS)) {
@@ -108,6 +111,7 @@ class HomeFragment : Fragment(), SensorEventListener {
     private fun stopMeasuringHeartRate() {
         sensorManager.unregisterListener(this)
         binding.textHeartRate.text = getString(R.string.text_heart_rate_idle)
+        binding.textHeartRate.textSize = 24F
         binding.textHeartRateAccuracy.text = ""
         binding.buttonMeasureHeartRate.text = getString(R.string.button_measure_heart_rate)
     }
