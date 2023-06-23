769fab71e6c3ba1e4ad56ffc9def6cf5428a4773
Eco-Gaming <7ecogaming4@gmail.com>
2023-06-23 11:39:45 +0200

    bottom bar now hides in settings and about fragment

--- app/src/main/java/me/ecogaming/heartratemonitor/MainActivity.kt
+++ app/src/main/java/me/ecogaming/heartratemonitor/MainActivity.kt
@@ -1,21 +1,27 @@
 package me.ecogaming.heartratemonitor
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUiSaveStateControl
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.preference.PreferenceManager
+import com.google.android.material.navigation.NavigationBarView
 import me.ecogaming.heartratemonitor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navView: NavigationBarView
     private lateinit var binding: ActivityMainBinding
 
+    @OptIn(NavigationUiSaveStateControl::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
@@ -35,11 +41,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        navView = binding.bottomNavigationView
+
         setSupportActionBar(binding.toolbar)
 
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.historyFragment,
+            )
+        )
+
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+        // Don't save the state of other main navigation items, only this experimental function allows turning off this behavior
+        NavigationUI.setupWithNavController(navView, navController, false)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigationView.visibility = when (destination.id) {
+                R.id.settingsFragment, R.id.aboutFragment -> View.GONE
+                else -> View.VISIBLE
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/me/ecogaming/heartratemonitor/fragments/HistoryFragment.kt
@@ -0,0 +1,36 @@
+package me.ecogaming.heartratemonitor.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import me.ecogaming.heartratemonitor.databinding.FragmentHistoryBinding
+
+class HistoryFragment : Fragment() {
+
+    private var _binding: FragmentHistoryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
