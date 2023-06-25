18c58370bee4cc5115b21e8b77344523946797e0
Eco-Gaming <7ecogaming4@gmail.com>
2023-06-25 13:25:46 +0200

    added frontend actions for history

--- app/src/main/java/me/ecogaming/heartratemonitor/fragments/HistoryFragment.kt
+++ app/src/main/java/me/ecogaming/heartratemonitor/fragments/HistoryFragment.kt
@@ -1,10 +1,18 @@
 package me.ecogaming.heartratemonitor.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import me.ecogaming.heartratemonitor.R
 import me.ecogaming.heartratemonitor.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
@@ -27,10 +35,41 @@ class HistoryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.menu_history, menu)
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                    return when (menuItem.itemId) {
+                        R.id.action_clear_history -> {
+                            clearHistory()
+                            true
+                        }
+                        else -> false
+                    }
+                }
+            },
+            viewLifecycleOwner, Lifecycle.State.RESUMED
+        )
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun clearHistory() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        builder.setTitle(R.string.clear_history)
+            .setMessage(R.string.clear_history_dialog_message)
+            .setPositiveButton(R.string.clear_history) { _, _ ->
+                // TODO: clear history
+            }
+            .setNegativeButton(R.string.cancel, null)
+            .show()
     }
 }
